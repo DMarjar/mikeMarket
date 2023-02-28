249936444da2612f342df7e862781c443b34283d
DMarjar <20213tn119@utez.edu.mx>
2023-02-28 12:57:26 -0600

    avance

--- src/modules/category/CategoryScreen.jsx
+++ src/modules/category/CategoryScreen.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState }from "react";
+import { Card, Col, Row } from "react-bootstrap";
+import { ButtonCircle } from "../../shared/components/ButtonCircle";
+import DataTable from "react-data-table-component";
 
 export const CategoryScreen = () => {
+
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(second)
+  const [filterText, setFilterText] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+  const [isEditing, setIsEditing] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <div>CategoryScreen</div>
-  )
-}
+    <Card>
+      <Card.Header>
+        <Row>
+          <Col>Categories</Col>
+          <Col className="text-end">
+            <ButtonCircle
+              type={"btn btn-outline-success"}
+              icon={"plus"}
+              size={16}
+              onClick={() => {
+                console.log("Add Category");
+              }}
+            />
+          </Col>
+        </Row>
+      </Card.Header>
+      <Card.Body>
+        <DataTable 
+          columns={[]}
+          data={categories}
+          progressPending={isLoading}
+          progressComponent={<>LOADING</>}
+          noDataComponent={"No data found"}
+          pagination
+          paginationComponentOptions={{}}
+          subHeader
+          subHeaderComponent={<></>}
+          persistTableHead
+          striped={true}
+          highlightOnHover={true}
+        />
+      </Card.Body>
+    </Card>
+  );
+};
---
+++ src/shared/components/ButtonCircle.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FeatherIcon } from "feather-icons-react";
+
+export const ButtonCircle = ({ type, onClick, icon = "", size = 15 }) => {
+  return (
+    <button className={type} onClick={onClick}>
+      {icon && (
+        <FeatherIcon icon={icon} size={size} style={{ strokeWidth: 2.5 }} />
+      )}
+    </button>
+  );
+};
